0cb90f12c7bf4fd2f1a6ed3e95d940ce8ffacd93
yuvarajchandirasekaran <yuvarajcdckap@gmail.com>
2023-11-15 11:39:10 +0530

    Completed

---
+++ index.js
@@ -0,0 +1,34 @@
+document.querySelector('.show-filter-btn-tab').addEventListener("click",()=>{
+
+    document.querySelector('.filter-container').classList.toggle("show");
+
+})
+
+// window.addEventListener('DOMContentLoaded', ()=>{
+//     document.querySelector('.list-view-btn').classList.add('active');
+//     document.querySelector('.products-list-view').classList.add('show')
+// })
+
+const btns=document.querySelectorAll('.view-btn')
+const productContainer = document.querySelectorAll('.products-view');
+const mainContainer = document.querySelector('.products-listing-container')
+
+mainContainer.addEventListener("click", function (e) {
+    const id = e.target.dataset.id;
+    if (id) {
+      // remove selected from other buttons
+      btns.forEach(function (btn) {
+        btn.classList.remove("active");
+      });
+      e.target.classList.add("active");
+      // hide other articles
+      productContainer.forEach(function (article) {
+        article.classList.remove("active");
+      });
+      const element = document.getElementById(id);
+      element.classList.add("active");
+    }
+  });
+
+
+
